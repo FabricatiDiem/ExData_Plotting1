378865a45c5475ff06635bb35044249973323f25
Jake Metzger <jmetzger@email.arizona.edu>
2017-10-15 22:27:53 -0700

    Corrected the swap of par() and png() that took place during earlier cleanup. plot4.R now correctly shows all four panels, as expected.

--- plot4.R
+++ plot4.R
@@ -13,11 +13,11 @@ dataset$Datetime = paste(dataset$Date, dataset$Time)
 dataset$Datetime = as.POSIXct(dataset$Datetime, format="%d/%m/%Y %H:%M:%S")
 dataset2 = subset(dataset, dataset$Datetime>='2007-02-01' & dataset$Datetime <'2007-02-03')
 
-#Set plot for multiple subplots in 2x2 frame
-par(mfrow=c(2,2))
-
 #Open output device
 png("plot4.png")
+
+#Set plot for multiple subplots in 2x2 frame
+par(mfrow=c(2,2))
 
 #Plot 1
 plot(x = dataset2$Datetime, 
